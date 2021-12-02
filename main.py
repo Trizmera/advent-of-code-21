bf6d17c35d2d67f5473b9f1483cd97f603388eaa
Beatriz Cruz <mllebia@live.com>
2021-12-01 21:11:27 -0300

    Day 01

---
+++ main.py
@@ -0,0 +1,6 @@
+with open('inputs/day_one.txt', 'r') as f:
+    sea_floor_depth = [int(number.strip()) for number in f.readlines()]
+
+depth_increased = sum([1 for i in range(1, len(sea_floor_depth)) if sea_floor_depth[i] > sea_floor_depth[i-1]])
+
+print("The number of times of the depth increases:", depth_increased)
