45c52d986c8258b0b48570fd0c51e0a512f39ce3
Beatriz Cruz <mllebia@live.com>
2021-12-01 22:47:26 -0300

    Day 01 Part 2

--- day_01.py
+++ day_01.py
@@ -5,3 +5,8 @@ with open('inputs/day_01.txt', 'r') as f:
 increased = sum([1 for i in range(1, len(depth)) if depth[i] > depth[i-1]])
 
 print("Number of times the depth increases:", increased)
+
+increased = sum([1 for i in range(3, len(depth)) if depth[i] > depth[i-3]])
+
+
+print("Number of times the sliding window:", increased)
